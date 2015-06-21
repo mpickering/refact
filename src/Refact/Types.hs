5fc6585cefb50a0fddba0f56cc8dabb9fad832e8
Matthew Pickering <matthewtpickering@gmail.com>
2015-06-21 19:27:39 +0100

    Add type of expression to refactor

--- src/Refact/Types.hs
+++ src/Refact/Types.hs
@@ -8,9 +8,12 @@ data SrcSpan = SrcSpan
                 { start :: (Int, Int)
                 , end :: (Int, Int) } deriving (Read, Show, Eq, Ord)
 
+data RType = Expr | Decl | Type | Pattern deriving (Read, Ord, Show, Eq)
+
 data Refactoring a =
   Replace  {
-      expr :: a  -- ^ Expression to replace
+      rtype :: RType -- ^ Type of expression to be replaced
+    , expr :: a  -- ^ Expression to replace
     , subts :: [(String, a)] -- ^ Substitutions to make
     , orig  :: String -- ^ Replacment template
     }
